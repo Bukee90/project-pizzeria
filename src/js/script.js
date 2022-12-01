8793d7a90bb78b5ee67ce12fb5c2bdd0331442a7
Dawid Bogusz <dawid.bogusz90.kursy@gmail.com>
2022-12-01 17:03:36 +0100

    modiffy JS files - something not works

--- src/js/script.js
+++ src/js/script.js
@@ -60,8 +60,10 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
 
+      
       console.log('new Product:', thisProduct);
     }
     renderInMenu() {
@@ -71,17 +73,69 @@
       const menuContainer = document.querySelector(select.containerOf.menu);
       menuContainer.appendChild(thisProduct.element);
     }
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
     initAccordion() {
       const thisProduct = this;
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-      clickableTrigger.addEventListener('click', function(event) {
+      // const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      // clickableTrigger.addEventListener('click', function(event) {
+      thisProduct.accordionTrigger.addEventListener('click', function(event) {
         event.preventDefault();
         const activeProduct = document.querySelector(select.all.menuProductsActive);
         if (activeProduct != thisProduct.element && activeProduct != null) {
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
-      })
+      });
+    }
+    initOrderFrom() {
+      const thisProduct = this;
+      console.log('initOrderFrom:', thisProduct);
+      thisProduct.form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      for (let input of thisProduct.formInputs) {
+        input.addEventListener('change', function() {
+          thisProduct.processOrder();
+        });
+      }
+      thisProduct.cartButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+    processOrder() {
+      const thisProduct = this;
+      console.log('procesOrder:', thisProduct);
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData:', formData)
+      let price = thisProduct.data.price;
+      for (let paramId in thisProduct.data.params) {
+        const param = thisProduct.data.params[paramId];
+        console.log(optionId, option);
+        for (let optionId in param.options) {
+          const option = param.options[optionId];
+          console.log('option:', optionId, option);
+          if (formData[paramId] && formData[paramId].includes(optionId)) {
+            if (!option.default == true) {
+              price += option.price;
+            }
+          } else {
+            if (!option.default == false) {
+              price -= option.price;
+            }
+          }
+        } 
+      }
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
@@ -92,7 +146,7 @@
       for (let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
       }
-      
+   
     },
     initData: function() {
       const thisApp = this;
